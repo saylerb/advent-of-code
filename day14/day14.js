2dff3aac8adfda44350935a6c9ba1df7a4c6340e
Brian Sayler <sayler.b@gmail.com>
2020-12-20 12:44:41 -0700

    day 14 part two - expand memory addresses out

--- day14/day14.js
+++ day14/day14.js
@@ -11,43 +11,138 @@ export function part1(fileName) {
   let memory = {};
 
   lines.forEach((line) => {
-    const [part1, part2] = line.split("=").map((part) => part.trim());
+    const [instruction, numToWrite] = line
+      .split("=")
+      .map((part) => part.trim());
 
-    if (part1 === "mask") {
+    if (instruction === "mask") {
       currentMask = line.substring(line.indexOf("=") + 2);
     }
 
-    if (part1.indexOf("mem") >= 0) {
-      const number = parseInt(part1.match(/\d+/g)[0]);
+    if (instruction.indexOf("mem") >= 0) {
+      const address = parseInt(instruction.match(/\d+/g)[0]);
 
-      memory[number] = applyMask(currentMask, parseInt(part2));
+      memory[address] = applyMaskToNumber(currentMask, parseInt(numToWrite));
     }
   });
 
   return Object.values(memory).reduce((acc, num) => (acc += num), 0);
 }
 
-export function part2() {}
+export function part2(fileName) {
+  const lines = parseFile(fileName);
 
-export function applyMask(mask, number) {
+  let currentMask;
+
+  let memory = {};
+
+  lines.forEach((line) => {
+    const [instruction, numToWrite] = line
+      .split("=")
+      .map((part) => part.trim());
+
+    if (instruction === "mask") {
+      currentMask = line.substring(line.indexOf("=") + 2);
+    }
+
+    if (instruction.indexOf("mem") >= 0) {
+      const address = parseInt(instruction.match(/\d+/g)[0]);
+
+      const expandedAddresses = applyMaskToAddress(
+        currentMask,
+        parseInt(address)
+      );
+
+      expandedAddresses.forEach((address) => {
+        memory[address] = parseInt(numToWrite);
+      });
+    }
+  });
+
+  return Object.values(memory).reduce((acc, num) => (acc += num), 0);
+}
+
+export function applyMaskToNumber(mask, number) {
+  const newNumber = applyMask(mask, number, part1MaskFunction);
+
+  return parseInt(newNumber, 2);
+}
+
+const part1MaskFunction = ([bit, maskValue]) => {
+  if (maskValue === "X") {
+    return bit;
+  }
+
+  return maskValue;
+};
+
+const part2MaskFunction = ([bit, maskValue]) => {
+  if (maskValue === "0") {
+    return bit;
+  }
+
+  if (maskValue === "1") {
+    return "1";
+  }
+
+  if (maskValue === "X") {
+    return "X";
+  }
+};
+
+export function applyMask(mask, number, fn = part1MaskFunction) {
   const binaryNumber = number.toString(2).padStart(36, "0");
 
   const reversedMask = mask.split("").slice(0).reverse();
   const reversedNumber = binaryNumber.split("").slice(0).reverse();
 
-  const result = reversedNumber.map((bit, index) => {
-    const maskValue = reversedMask[index];
-
-    if (maskValue === "X") {
-      return bit;
-    }
-
-    return reversedMask[index];
+  const zipped = reversedNumber.map((bit, index) => {
+    return [bit, reversedMask[index]];
   });
 
-  const bin = result.slice(0).reverse().join("");
+  const result = zipped.map(fn);
 
-  return parseInt(bin, 2);
+  const joined = result.slice(0).reverse().join("");
+
+  return joined;
+}
+
+export function applyMaskToAddress(mask, number) {
+  const newNumber = applyMask(mask, number, part2MaskFunction);
+
+  if (newNumber.indexOf("X") === -1) {
+    return newNumber;
+  }
+
+  const floatingIndices = [];
+  let index = newNumber.indexOf("X");
+  while (index != -1) {
+    floatingIndices.push(index);
+
+    index = newNumber.indexOf("X", index + 1);
+  }
+
+  let expandedNumbers = [newNumber.slice(0)];
+
+  for (let i = 0; i < floatingIndices.length; i++) {
+    let index = floatingIndices[i];
+
+    expandedNumbers = expandedNumbers.flatMap((expanded) => {
+      let result = [];
+      let copy = [...expanded];
+
+      copy[index] = "1";
+
+      result.push(copy.join(""));
+      copy[index] = "0";
+
+      result.push(copy.join(""));
+
+      return result;
+    });
+  }
+
+  return expandedNumbers.map((expanded) => parseInt(expanded, 2));
 }
 
 export function parseFile(filename) {
--- day14/day14.test.js
+++ day14/day14.test.js
@@ -1,10 +1,23 @@
-import { part1, part2, parseFile, applyMask } from "./day14.js";
+import {
+  part1,
+  part2,
+  parseFile,
+  applyMask,
+  applyMaskToAddress,
+  applyMaskToNumber,
+} from "./day14.js";
 
 describe("part 1", () => {
   test("take a base 10 number and a mask and get an output in base 10", () => {
-    expect(applyMask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 11)).toEqual(73);
-    expect(applyMask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 101)).toEqual(101);
-    expect(applyMask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 0)).toEqual(64);
+    expect(
+      applyMaskToNumber("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 11)
+    ).toEqual(73);
+    expect(
+      applyMaskToNumber("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 101)
+    ).toEqual(101);
+    expect(
+      applyMaskToNumber("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", 0)
+    ).toEqual(64);
   });
 
   test("test part 1", () => {
@@ -22,7 +35,13 @@ describe("part 1", () => {
 });
 
 describe("part 2", () => {
+  test("apply mask to base 10 memory address", () => {
+    expect(
+      applyMaskToAddress("000000000000000000000000000000X1001X", 42)
+    ).toEqual(expect.arrayContaining([26, 27, 58, 59]));
+  });
   test("test part 2", () => {
-    expect(part2()).toBeUndefined();
+    expect(part2("test2.txt")).toEqual(208);
+    expect(part2("day14input.txt")).toEqual(3608464522781);
   });
 });
