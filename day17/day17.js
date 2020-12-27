a5cffcb728344a379f963eac18d1bfa7a9bd1c90
Brian Sayler <sayler.b@gmail.com>
2020-12-26 18:28:58 -0700

    day17 part 1 - initial solution

--- day17/day17.js
+++ day17/day17.js
@@ -3,17 +3,276 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export function part1() {}
+export function part1(fileName) {
+  const lines = parseFile(fileName);
+
+  const pocket = createPocketDimension(lines);
+
+  for (let i = 0; i < 6; i++) {
+    pocket.next();
+  }
+
+  return pocket.totalActive();
+}
 
 export function part2() {}
 
-export function getSurroundingCubes({ x, y, z }) {
+export class Point {
+  constructor({ x, y, z }) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}
+
+export class Cube {
+  constructor({ point, active }) {
+    this.point = point;
+    this.active = active;
+  }
+
+  printState() {
+    if (this.active) {
+      return "#";
+    } else {
+      return ".";
+    }
+  }
+}
+
+export class PocketDimension {
+  constructor(cubes) {
+    this._cubeMap = this.createCubeMap(cubes);
+  }
+
+  get cubeMap() {
+    return this._cubeMap;
+  }
+
+  createCubeMap(inputCubes) {
+    const theMap = new Map();
+
+    inputCubes.forEach((cube) => {
+      theMap.set(JSON.stringify(cube.point), cube);
+    });
+
+    return theMap;
+  }
+
+  nextCubeState(trackedCubes, cube) {
+    const totalActiveSurroundingCubes = trackedCubes.reduce((acc, cube) => {
+      if (cube.active) {
+        acc++;
+      }
+      return acc;
+    }, 0);
+
+    if (cube.active) {
+      if (
+        totalActiveSurroundingCubes === 2 ||
+        totalActiveSurroundingCubes === 3
+      ) {
+        // cube remains active
+        return cube;
+      } else {
+        // cube shuts off
+        return new Cube({ point: cube.point, active: false });
+      }
+    }
+    if (!cube.active) {
+      if (totalActiveSurroundingCubes === 3) {
+        return new Cube({ point: cube.point, active: true });
+      } else {
+        // cube remains inactive
+        return cube;
+      }
+    }
+  }
+
+  next() {
+    const trackedMap = new Map();
+
+    let total = 0;
+
+    const untrackedMap = new Map();
+    // first go through each cube and gather cubes not tracked
+
+    for (const [key, value] of this._cubeMap) {
+      // console.log(value.point);
+      const { tracked, untracked } = this.getSurroundingCubes(value.point);
+
+      const nextCube = this.nextCubeState(tracked, value);
+
+      trackedMap.set(key, nextCube);
+
+      untracked.forEach((cube) => {
+        untrackedMap.set(JSON.stringify(cube.point), cube);
+      });
+    }
+
+    for (const [key, value] of untrackedMap) {
+      // console.log(value.point);
+      const { tracked, untracked } = this.getSurroundingCubes(value.point);
+
+      const nextCube = this.nextCubeState(tracked, value);
+
+      trackedMap.set(key, nextCube);
+    }
+
+    this._cubeMap = trackedMap;
+  }
+
+  getCubeAtPoint(point) {
+    return this._cubeMap.get(JSON.stringify(point));
+  }
+
+  totalCubes() {
+    return this._cubeMap.size;
+  }
+
+  totalActive() {
+    let total = 0;
+    for (const [_, value] of this._cubeMap) {
+      if (value.active) [total++];
+    }
+
+    return total;
+  }
+
+  totalInactive() {
+    let total = 0;
+    for (const [_, value] of this._cubeMap) {
+      if (!value.active) [total++];
+    }
+
+    return total;
+  }
+
+  addInactiveCubeAtPoint(point) {
+    this._cubeMap.set(
+      JSON.stringify(point),
+      new Cube({ point, active: false })
+    );
+  }
+
+  getSurroundingCubes(point) {
+    // console.log({ point });
+    const coords = getSurroundingCubeCoords({
+      x: point.x,
+      y: point.y,
+      z: point.z,
+    });
+    // console.log("finding cubes");
+    // let inActiveCubesCreated = 0;
+
+    const tracked = [];
+    const untracked = [];
+
+    const cubes = coords.forEach(({ x, y, z }) => {
+      const point = new Point({ x, y, z });
+
+      const searchResult = this.getCubeAtPoint(point);
+
+      if (typeof searchResult === "undefined") {
+        untracked.push(new Cube({ point, active: false }));
+      } else {
+        tracked.push(searchResult);
+      }
+    });
+
+    // console.log(`${untracked.length} cubes not found`);
+
+    return { tracked, untracked };
+  }
+
+  printCubeArray() {
+    const result = [];
+
+    for (const value of this._cubeMap.values()) {
+      if (typeof result[value.point.z] !== "undefined") {
+        result[value.point.z] = result[value.point.z].concat(value);
+      } else {
+        result[value.point.z] = [value];
+      }
+    }
+
+    return result
+      .map((z) => {
+        // List of Cubes at z index
+
+        const cubesByYIndex = [];
+
+        z.forEach((cube) => {
+          if (typeof cubesByYIndex[cube.point.y] !== "undefined") {
+            cubesByYIndex[cube.point.y] = cubesByYIndex[cube.point.y].concat(
+              cube
+            );
+          } else {
+            cubesByYIndex[cube.point.y] = [cube];
+          }
+        });
+
+        return cubesByYIndex.map((rows) =>
+          // Sort each row of Y by x coordinate
+          rows.sort((cubeA, cubeB) => cubeA.point.x - cubeB.point.x)
+        );
+      })
+      .flatMap((zDimensions) => {
+        return zDimensions.map((yDimensions) => {
+          return yDimensions
+            .map((cube) => {
+              if (cube.active) {
+                return "#";
+              } else {
+                return ".";
+              }
+            })
+            .join("");
+        });
+      });
+
+    // return { totalZ, totalY, totalX };
+    for (const [_, value] of this._cubeMap) {
+      result.push(value);
+    }
+    return result;
+  }
+}
+function groupByArray(xs, key) {
+  return xs.reduce(function (rv, x) {
+    let v = key instanceof Function ? key(x) : x[key];
+    let el = rv.find((r) => r && r.key === v);
+    if (el) {
+      el.values.push(x);
+    } else {
+      rv.push({ key: v, values: [x] });
+    }
+    return rv;
+  }, []);
+}
+
+export function createPocketDimension(lines) {
+  const cubes = lines.flatMap((row, y) => {
+    return row.split("").map((xValue, x) => {
+      return new Cube({
+        point: new Point({ x, y, z: 0 }),
+        active: xValue === "#",
+      });
+    });
+  });
+
+  return new PocketDimension(cubes);
+}
+
+export function processInitialState(initialState) {}
+
+export function getSurroundingCubeCoords({ x, y, z }) {
   const result = [];
   for (let newX = x - 1; newX <= x + 1; newX++) {
     for (let newY = y - 1; newY <= y + 1; newY++) {
       for (let newZ = z - 1; newZ <= z + 1; newZ++) {
-        const notCurrentCube = newX.x !== x && newY.y !== y && newZ.z !== z;
-        if (notCurrentCube) {
+        const isCurrentCube = newX === x && newY === y && newZ === z;
+        if (!isCurrentCube) {
           result.push({ x: newX, y: newY, z: newZ });
         }
       }
--- day17/day17.test.js
+++ day17/day17.test.js
@@ -1,11 +1,28 @@
-import { part1, part2, parseFile, getSurroundingCubes } from "./day17.js";
+import {
+  part1,
+  part2,
+  parseFile,
+  getSurroundingCubeCoords,
+  Cube,
+  Point,
+  createPocketDimension,
+} from "./day17.js";
 
 describe("part 1", () => {
   test("can get a list of coordinates of surrounding cubes", () => {
-    const cubes = "thing";
-
     // Should be 26 total "cubes"
-    expect(getSurroundingCubes({ x: 0, y: 0, z: 0 })).toEqual(
+    const result = getSurroundingCubeCoords({ x: 0, y: 0, z: 0 });
+    expect(result.length).toEqual(26);
+    expect(result).not.toEqual(
+      expect.arrayContaining([
+        {
+          x: 0,
+          y: 0,
+          z: 0,
+        },
+      ])
+    );
+    expect(result).toEqual(
       expect.arrayContaining([
         // z = 0
         { x: 0, y: 1, z: 0 }, //  N
@@ -42,12 +59,92 @@ describe("part 1", () => {
     );
   });
 
+  test("can create an active cube that can print itself", () => {
+    const active = new Cube({ x: 0, y: 0, z: 0, active: true });
+
+    expect(active.printState()).toEqual("#");
+  });
+
+  test("create initial cubes from input", () => {
+    const initalState = [".#.", "..#", "###"];
+
+    const pocket = createPocketDimension(initalState);
+
+    expect(pocket.totalCubes()).toEqual(9);
+    expect(pocket.totalActive()).toEqual(5);
+    expect(pocket.totalInactive()).toEqual(4);
+
+    expect(pocket.cubeMap.size).toEqual(9);
+    const point = new Point({ x: 0, y: 0, z: 0 });
+    const stringifiedPoint = JSON.stringify(point);
+    expect(pocket.cubeMap.get(stringifiedPoint)).toEqual(
+      new Cube({ point, active: false })
+    );
+
+    expect(pocket.getCubeAtPoint(new Point({ x: 0, y: 0, z: 0 }))).toEqual(
+      new Cube({ point, active: false })
+    );
+
+    expect(pocket.getCubeAtPoint(new Point({ x: 10, y: 10, z: 10 }))).toEqual(
+      undefined
+    );
+
+    expect(pocket.printCubeArray()).toEqual(initalState);
+  });
+
+  test("can get all the surround cubes for a specific point", () => {
+    const pocket = createPocketDimension([".#.", "..#", "###"]);
+
+    const { tracked, untracked } = pocket.getSurroundingCubes(
+      new Point({ x: 0, y: 0, z: 0 })
+    );
+    expect(tracked.length).toEqual(3);
+    expect(untracked.length).toEqual(23);
+  });
+
+  test("can run the rules against initial state", () => {
+    const pocket = createPocketDimension([".#.", "..#", "###"]);
+
+    pocket.next();
+    expect(pocket.totalActive()).toEqual(11);
+
+    pocket.next();
+    expect(pocket.totalActive()).toEqual(21);
+
+    pocket.next();
+    expect(pocket.totalActive()).toEqual(38);
+
+    pocket.next();
+    pocket.next();
+    pocket.next();
+    expect(pocket.totalActive()).toEqual(112);
+  });
+
+  test("can run rules against input", () => {
+    const pocket = createPocketDimension([".#.", "..#", "###"]);
+
+    pocket.next();
+    expect(pocket.totalActive()).toEqual(11);
+
+    pocket.next();
+    expect(pocket.totalActive()).toEqual(21);
+
+    pocket.next();
+    expect(pocket.totalActive()).toEqual(38);
+
+    pocket.next();
+    pocket.next();
+    pocket.next();
+    expect(pocket.totalActive()).toEqual(112);
+  });
+
   test("test part 1", () => {
-    expect(part1()).toBeUndefined();
+    expect(part1("test.txt")).toEqual(112);
+    expect(part1("day17input.txt")).toEqual(269);
   });
   test("test parsing of file", () => {
     expect(parseFile("test.txt")).toEqual(
-      expect.arrayContaining(["Hello", "World"])
+      expect.arrayContaining([".#.", "###"])
     );
   });
 });
