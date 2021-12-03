277b5ecaae9fd228896772357bc73b5ea87a6ba8
Brian Sayler <sayler.b@gmail.com>
2021-12-02 17:22:38 -0700

    turn back on strict mode

--- 2021/day01/parseFile.ts
+++ 2021/day01/parseFile.ts
@@ -3,9 +3,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export function parseFile(filename) {
-  let data;
-  let rows;
+export function parseFile(filename: string): string[] {
+  let data: string;
+  let rows: string[] = [];
 
   try {
     data = fs.readFileSync(path.resolve(__dirname, filename), "utf8");
