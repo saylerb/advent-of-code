b384158a54dbf6426a9b7db05312961a18c85fea
Brian Sayler <sayler.b@gmail.com>
2021-12-08 20:23:26 -0700

    unskip some of the tests for day04

--- 2021/day04/index.test.ts
+++ 2021/day04/index.test.ts
@@ -1,9 +1,6 @@
 import { parseBingo, Board, Game } from "./index.js";
 
-describe("part 1", () => {
-  test("test part 1", () => {
-    // expect(part1()).toBeUndefined();
-  });
+describe("bingo", () => {
   test("test parsing of file", () => {
     expect(parseBingo("test.txt")).toEqual(
       expect.objectContaining({
@@ -36,33 +33,6 @@ describe("part 1", () => {
 
     expect(board.hasWon()).toBe(false);
   });
-
-  // test("a board should know if it has won", () => {
-  //   const board = new Board([
-  //     [22, 13, 17, 11, 0],
-  //     [8, 2, 23, 4, 24],
-  //     [21, 9, 14, 16, 7],
-  //     [6, 10, 3, 18, 5],
-  //     [1, 12, 20, 15, 19],
-  //   ]);
-
-  // Index for first two rows
-  // expect(board.index).toEqual({
-  //   22: { row: 0, col: 0, marked: false },
-  //   13: { row: 0, col: 1, marked: false },
-  //   17: { row: 0, col: 2, marked: false },
-  //   11: { row: 0, col: 3, marked: false },
-  //   0: { row: 0, col: 4, marked: false },
-  //   8: { row: 1, col: 0, marked: false },
-  //   2: { row: 1, col: 1, marked: false },
-  //   23: { row: 1, col: 2, marked: false },
-  //   4: { row: 1, col: 3, marked: false },
-  //   24: { row: 1, col: 4, marked: false },
-  // });
-  //   board.addDrawing(22);
-
-  //   expect(board.numbersMarked()).toEqual(1);
-  // });
 
   test("a board can index its numbers into a map for easy checking", () => {
     const board = new Board([[22, 13, 17, 11, 0]]);
@@ -232,7 +202,7 @@ describe("part 1", () => {
     expect(result).toEqual(expectedScore);
   });
 
-  test.only("provided example", () => {
+  test("provided example", () => {
     const drawings = [
       7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22,
       18, 20, 8, 19, 3, 26, 1,
@@ -269,34 +239,5 @@ describe("part 1", () => {
     const result = game.playUntilSingleBoardLeft();
 
     expect(result).toEqual(1924);
-  });
-
-  // test("can calculate the board score", () => {
-  //   const boardOne = new Board([
-  //     [22, 13],
-  //     [8, 2],
-  //   ]);
-
-  //   const boardTwo = new Board([
-  //     [6, 10],
-  //     [1, 12],
-  //   ]);
-
-  //   const drawings = [2, 13];
-
-  //   const game = new Game([boardOne, boardTwo], drawings);
-
-  //   const result = game.play();
-
-  //   const expectedScore: number = 13 * (2 + 13);
-  //   expect(game.someoneWon).toEqual(true);
-
-  //   expect(result).toEqual(expectedScore);
-  // });
-});
-
-describe("part 2", () => {
-  test("test part 2", () => {
-    // expect(part2()).toBeUndefined();
   });
 });
